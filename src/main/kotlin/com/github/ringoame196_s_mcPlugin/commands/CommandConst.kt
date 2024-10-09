992100fde7b5d47dd414e255033f6d2a72ea8e59
りんご飴196 <kazumac41@gmail.com>
2024-10-09 22:10:36 +0900

    feat:tabcompleterにサブコマンドなどを表示

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/CommandConst.kt
@@ -0,0 +1,8 @@
+package com.github.ringoame196_s_mcPlugin.commands
+
+object CommandConst {
+    const val SEND_COMMAND = "send" // 送るサブコマンド
+    const val DISPLAY_COMMAND = "display" // 表示サブコマンド
+    const val BLOCK_COMMAND = "block" // ブロックコマンド
+    const val UN_BLOCK_COMMAND = "unblock" // ブロック解除コマンド
+}
--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/TabCompleter.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/TabCompleter.kt
@@ -6,6 +6,17 @@ import org.bukkit.command.TabCompleter
 
 class TabCompleter : TabCompleter {
     override fun onTabComplete(commandSender: CommandSender, command: Command, label: String, args: Array<out String>): MutableList<String>? {
-        return mutableListOf()
+        return when(args.size) {
+            1 -> mutableListOf(CommandConst.SEND_COMMAND, CommandConst.DISPLAY_COMMAND, CommandConst.BLOCK_COMMAND, CommandConst.UN_BLOCK_COMMAND)
+            2 -> when(args[0]) {
+                CommandConst.SEND_COMMAND -> mutableListOf("[件名]")
+                else -> mutableListOf()
+            }
+            3 -> when(args[1]) {
+                CommandConst.SEND_COMMAND -> mutableListOf("[本文]")
+                else -> mutableListOf()
+            }
+            else -> mutableListOf()
+        }
     }
 }
